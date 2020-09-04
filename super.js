62f4d9a370a45bb8c0d46594da4ba3d04f928863
Juniardi Akbar <juniardiakbar@gmail.com>
2020-09-04 10:43:32 +0700

    Fix encrypt and add decrypt using super cipher

--- super.js
+++ super.js
@@ -1,21 +1,58 @@
 const { encryptVigenere, decryptVigenere } = require("./vigenere");
-const { mod, intsToCharList } = require("./helper");
+const { div, mod, intsToCharList } = require("./helper");
 const K = 4;
 
-const transposeEncrypt = (cipher) => {
+const transposeDecrypt = (chiper) => {
   const output = [];
-  let j = 0;
+  const matrix = [];
+  let index = 0;
 
-  for (let i = 0; i < cipher.length; i++) {
-    output[i] = cipher[j];
-    j += K;
+  for (let i = 0; i < K; i++) {
+    let numRow = div(chiper.length, K);
+    if (i < mod(chiper.length, K)) numRow++;
 
-    if (j >= cipher.length) {
-      j++;
-      j = mod(j, K);
+    const temp = [];
+    for (let j = 0; j < numRow; j++) {
+      temp.push(chiper[index]);
+      index++;
     }
+
+    matrix.push(temp);
   }
 
+  index = 0;
+  for (let i = 0; i < div(chiper.length, K) + 1; i++) {
+    for (let j = 0; j < K; j++) {
+      if (matrix[j][i] != null) {
+        output[index] = matrix[j][i];
+        index++;
+      }
+    }
+  }
+  return output;
+};
+
+const transposeEncrypt = (plaintext) => {
+  const output = [];
+  const matrix = [];
+
+  for (let i = 0; i < div(plaintext.length, K) + 1; i++) {
+    const temp = [];
+    for (let j = 0; j < K; j++) {
+      temp.push(plaintext[i * K + j]);
+    }
+    matrix.push(temp);
+  }
+
+  let index = 0;
+  for (let i = 0; i < K; i++) {
+    for (let j = 0; j < div(plaintext.length, K) + 1; j++) {
+      if (matrix[j][i] != null) {
+        output[index] = matrix[j][i];
+        index++;
+      }
+    }
+  }
   return output;
 };
 
