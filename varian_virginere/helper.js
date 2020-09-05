07c34bae39c93eaadf8a2a5300159e1579604a57
irfansofyana <irfansofyana0305@gmail.com>
2020-09-05 09:08:25 +0700

    Add full_viginere and unit testing

---
+++ varian_virginere/helper.js
@@ -0,0 +1,71 @@
+const swapString = (string, a, b) => {
+    let arrString = string.split('');
+    [arrString[a], arrString[b]] = [arrString[b], arrString[a]];
+    return arrString.join('');
+}
+
+const shuffleString = (string) => {
+    let array = string;
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        array = swapString(array, i, j);
+    }
+    return array;
+}
+
+const generateMatrix = () => {
+    let alphabet = ''
+    for (let i = 0; i < 26; ++i) {
+        alphabet = alphabet + (String.fromCharCode('A'.charCodeAt() + i));
+    }
+
+    let key = [];
+    for (let i = 0; i < 26; ++i){
+        const shuffledString = shuffleString(alphabet);
+        key.push(shuffledString);
+    }
+
+    return key;
+}
+
+const findEncryptedChar = (matrix, char, key) => {
+    let plainTextPosition = -1;
+    for (let i = 0; i < 26 && plainTextPost === -1; ++i) {
+        if (matrix[0][i] === char) {
+            plainTextPosition = i;
+        }
+    }
+
+    let keyPositiion = -1;
+    for (let i = 0; i < 26 && keyPositiion === -1; ++i) {
+        if (matrix[i][0] === key) {
+            keyPositiion = i;
+        }
+    }
+
+    return matrix[keyPositiion][plainTextPosition];
+}
+
+const findDecryptedChar = (matrix, char, key) => {
+    let keyPosition = -1;
+    for (let i = 0; i < 26 && keyPosition === -1; ++i) {
+        if (matrix[i][0] === key) {
+            keyPosition = i;
+        }
+    }
+
+    let cipherposition = -1;
+    for (let i = 0; i < 26 && cipherposition === -1; ++i) {
+        if (matrix[keyPosition][i] === char) {
+            cipherposition = i;
+        }
+    }
+
+    return matrix[0][cipherposition];
+}
+
+module.exports = {
+    generateMatrix,
+    findEncryptedChar,
+    findDecryptedChar
+}
