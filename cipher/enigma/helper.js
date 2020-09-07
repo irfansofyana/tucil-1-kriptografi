728baef70b8e8e1fe289d57daee1496b872321dd
irfansofyana <irfansofyana0305@gmail.com>
2020-09-07 17:41:58 +0700

    change random number algorithm

--- cipher/enigma/helper.js
+++ cipher/enigma/helper.js
@@ -1,8 +1,11 @@
+const seedrandom = require('seedrandom');
+const rng = seedrandom('this_is_seed');
+
 const randomAlphabetOrder = () => {
     let arrayCopy = [...Array(26 + 1).keys()].slice(1);
 
     for (let i = arrayCopy.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
+        const j = Math.floor(rng() * (i + 1));
         [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
     }
     return arrayCopy;
--- cipher/extended_vigenere/helper.js
+++ cipher/extended_vigenere/helper.js
@@ -1,3 +1,6 @@
+const seedrandom = require('seedrandom');
+const rng = seedrandom('this_is_seed');
+
 const completeAutoKeyStandard = (key, plaintext) => {
     let newKey = key;
     for (let i = 0; i < plaintext.length - key.length; ++i) {
@@ -8,10 +11,10 @@ const completeAutoKeyStandard = (key, plaintext) => {
 }
 
 const randomKey = (plaintext) => {
-    const randomKeyLen = Math.floor(Math.random() * plaintext.length) + 1;
+    const randomKeyLen = Math.floor(rng() * plaintext.length) + 1;
     let key = '';
     for (let i = 0; i< randomKeyLen; ++i) {
-        const randomChar = String.fromCharCode(Math.floor(Math.random() * 256));
+        const randomChar = String.fromCharCode(Math.floor(rng() * 256));
         key = key + randomChar;
     }
 
