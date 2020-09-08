1ee5e88c2c6ea7ac9383699ca4e944eae3b5b658
irfansofyana <irfansofyana0305@gmail.com>
2020-09-09 02:15:31 +0700

    Cleaning files

--- cipher/extended_vigenere/index.js
+++ cipher/extended_vigenere/index.js
@@ -1,25 +1,3 @@
-// const encrypt = (plaintext, key) => {
-//   let resultEncryption = "";
-//   for (let i = 0; i < plaintext.length; ++i) {
-//     const order =
-//       (plaintext[i].charCodeAt(0) + key[i % key.length].charCodeAt(0)) % 256;
-//     resultEncryption = resultEncryption + String.fromCharCode(order);
-//   }
-
-//   return resultEncryption;
-// };
-
-// const decrypt = (cipher, key) => {
-//   let resultDecryption = "";
-//   for (let i = 0; i < cipher.length; ++i) {
-//     const order =
-//       (cipher[i].charCodeAt(0) - key[i % key.length].charCodeAt(0) + 256) % 256;
-//     resultDecryption = resultDecryption + String.fromCharCode(order);
-//   }
-
-//   return resultDecryption;
-// };
-
 const { mod, intListToText, textToIntList } = require("../helper");
 
 /*
@@ -33,8 +11,6 @@ exports.encrypt = (plaintext, key) => {
 
   for (let i = 0; i < pt.length; i++) {
     const k = kt[i % kt.length] - 97;
-    // if (i < 500) console.log(pt[i], k, mod(pt[i] + k, 256));
-
     output.push(mod(pt[i] + k, 256)); // Add 65 to make it upper case
   }
 
@@ -52,8 +28,6 @@ exports.decrypt = (cipher, key) => {
   for (let i = 0; i < ct.length; i++) {
     const k = kt[i % kt.length] - 97;
     output.push(mod(ct[i] - k, 256)); // Add 65 to make it upper case
-
-    // if (i < 500) console.log(mod(ct[i] - k, 256), k, ct[i]);
   }
 
   return intListToText(output);
