07d9b497d15c6e4481981cb6d4e263bc0db109c4
irfansofyana <irfansofyana0305@gmail.com>
2020-09-08 20:30:13 +0700

    Fix extended vigenere; add validation for enigma

--- cipher/extended_vigenere/index.js
+++ cipher/extended_vigenere/index.js
@@ -1,7 +1,7 @@
 const encrypt = (plaintext, key) => {
     let resultEncryption = '';
     for (let i = 0; i < plaintext.length; ++i) {
-        const order = (plaintext[i].charCodeAt(0) + key[i].charCodeAt(0)) % 256;
+        const order = (plaintext[i].charCodeAt(0) + key[i%key.length].charCodeAt(0)) % 256;
         resultEncryption = resultEncryption + String.fromCharCode(order);
     }
 
@@ -11,7 +11,7 @@ const encrypt = (plaintext, key) => {
 const decrypt = (cipher, key) => {
     let resultDecryption = '';
     for (let i = 0; i < cipher.length; ++i) {
-        const order = (cipher[i].charCodeAt(0) - key[i].charCodeAt(0) + 256) % 256;
+        const order = (cipher[i].charCodeAt(0) - key[i%key.length].charCodeAt(0) + 256) % 256;
         resultDecryption = resultDecryption + String.fromCharCode(order);
     }
 
